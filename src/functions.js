1542ccd35344ad56408652036f3e73635388d0b6
WesleybGuedes <wesleyguedes2013@gmail.com>
2024-03-13 15:06:55 -0300

    funtion palindrome, high and small implemented

--- src/functions.js
+++ src/functions.js
@@ -1,8 +1,33 @@
 // Requisito 1 - Crie a função verifyPalindromes
+verifyPalindrome = (string) => {
+  let reverseString = string.split('').reverse().join('');
+  if (string === reverseString) {
+    return true;
+  }
+  return false;
+};
 
 // Requisito 2 - Crie a função getHighestIndex
+getHighestIndex = (arrayInt) => {
+  let highestIndex = 0;
+  for (let i = 0; i < arrayInt.length; i += 1) {
+    if (arrayInt[i] > arrayInt[highestIndex]) {
+      highestIndex = i;
+    }
+  }
+  return highestIndex;
+};
 
 // Requisito 3 - Crie a função getSmallestIndex
+getSmallestIndex = (arrayInt) => {
+  let smallerIndex = 0;
+  for (let i = 0; i < arrayInt.length; i += 1) {
+    if (arrayInt[i] < arrayInt[smallerIndex]) {
+      smallerIndex = i;
+    }
+  }
+  return smallerIndex;
+};
 
 // Requisito 4 - Crie a função getLongestWord
 
@@ -18,3 +43,9 @@ module.exports = {
     ? countHighestNumberMaxOccurrences
     : (() => { }),
 };
+
+console.log(verifyPalindrome('arara'));
+
+console.log(getHighestIndex([2, 3, 6, 7, 10, 1]));
+
+console.log(getSmallestIndex([2, 4, 6, 7, 10, 0, -3]));
